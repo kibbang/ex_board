d9bd469109de6cee3b2aa0458e4c2dce21e06dbc
kihyun12 <81129734+kihyun12@users.noreply.github.com>
2022-05-04 18:46:58 +0900

    post show modify

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,7 +70,8 @@ class PostController extends Controller
     public function show($id)
     {
         //
-        $post = Post::findOrFail($id);
+        $post = Post::where('id', $id)->first();
+
         return $post;
     }
 
